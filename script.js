17c88ee94f46193a854d90410b2e11963d42ab97
Chamikar17 <gitseasen@gmail.com>
2025-05-31 07:53:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ watchBtn.addEventListener("click", () => {
     alert("Please close the previous ad before starting a new one.");
     return;
   }
-alret("තප්පර 15-17 අට්ගර කාලයක් ad එක නරබා මෙතනට පැමිනෙන්න.")
+
   adWindow = window.open(
     "https://www.profitableratecpm.com/x1fu9w31j?key=9e56e55a3a3f57fab750d1c7a4c9fac4",
     "_blank",
@@ -56,30 +56,3 @@ function addPoint(){
   points++;
   pointsSpan.textContent = points;
 }
-
-
-watchAdBtn.addEventListener("click", () => {
-  watchAdBtn.disabled = true;
-  let width = 0;
-  const interval = setInterval(() => {
-    if (width >= 100) {
-      clearInterval(interval);
-      points += 10;
-      pointsDisplay.innerText = points;
-      setTimeout(() => {
-        location.reload(); // reload the page after 1 second
-      }, 1000);
-    } else {
-      width += 1;
-      progressBar.style.width = width + "%";
-    }
-  }, 30); // animation speed
-});
-
-// WITHDRAW O
-
-
-
-
-
-
